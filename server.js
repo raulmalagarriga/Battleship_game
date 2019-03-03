85ec67dea8880160a26625e22de58555fcc6875e
raulmalagarriga <raulmalagarriga@hotmail.com>
2019-03-03 11:37:52 -0400

    3
    Probando servidor node

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var Entities = require('html-entities').AllHtmlEntities;
+var entities = new Entities();
+
+var port = 8900;
+
+app.use(express.static(__dirname + '/public'));
+
+http.listen(port, function(){
+    console.log('listening on *:' + port);
+});
